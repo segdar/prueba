290232592bd331d1ba40a8d64657d8416624daf3
Darwin segura <seg_44@hotmail.es>
2017-03-31 06:05:49 -0600

    prueba

---
+++ archivo.py
@@ -0,0 +1,15 @@
+#B = A
+#for i in range(10):
+#print i
+#x=2
+#while x < 10:
+ #   print x
+  #  x+=1
+  
+import urlib2 
+try: 
+	f= urlib2.urlope("[http://platzi.com"](http://platzi.com))
+print f.read()
+f.close()
+except:
+	print "Error"
